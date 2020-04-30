0a210f3853e004c5229667b15e6fa42dc37a90c2
testalucida <martin.kendel@rotmax.de>
2020-04-30 10:29:22 +0200

    added variadic function FlxStatusBox::setStatusTextV

--- fltk_ext/FlxStatusBox.h
+++ fltk_ext/FlxStatusBox.h
@@ -10,16 +10,22 @@
 #ifndef FLTK_EXT_FLXSTATUS_H_
 #define FLTK_EXT_FLXSTATUS_H_
 
+#include <string>
+
 class FlxStatusBox : public Fl_Box {
 public:
 	FlxStatusBox( int x, int y, int w, int h );
 	/** Set status text and time to elapse until reset.
 	 * The passed text will be copied. */
 	void setStatusText( const char* text, float time_to_reset = 3.0 );
+	void setStatusText( const std::string& text, float time_to_reset = 3.0 );
+	void setStatusTextV( const char* fmt... );
 	virtual ~FlxStatusBox();
 private:
 	static void resetStatus_static( void* );
 	void resetStatus();
+private:
+	std::string _temp;
 };
 
 #endif /* FLTK_EXT_FLXSTATUS_H_ */
--- src/FlxStatusBox.cpp
+++ src/FlxStatusBox.cpp
@@ -7,6 +7,9 @@
 
 #include <fltk_ext/FlxStatusBox.h>
 #include <FL/Fl.H>
+#include <cstdarg>
+
+using namespace std;
 
 FlxStatusBox::FlxStatusBox( int x, int y, int w, int h )
 : Fl_Box( x, y, w, h )
@@ -17,8 +20,41 @@ FlxStatusBox::FlxStatusBox( int x, int y, int w, int h )
 FlxStatusBox::~FlxStatusBox() {
 }
 
+void FlxStatusBox::setStatusTextV( const char* fmt... ) {
+	va_list args;
+	va_start( args, fmt );
+	_temp.clear();
+
+	while( *fmt != 0x00 ) {
+		if( *fmt == 'c' ) {
+			int c = va_arg( args, int );
+			_temp.append( 1, (char)c );
+		} else if( *fmt == 's' ) {
+			const char* cs = va_arg( args, const char* );
+			_temp.append( cs );
+		} else if( *fmt == 'd' ) {
+			int i = va_arg( args, int );
+			_temp.append( to_string( i ) );
+		} else if( *fmt == 'f' ) {
+			double d = va_arg( args, double );
+			_temp.append( to_string( d ) );
+		}
+		++fmt;
+
+	}
+	va_end( args );
+	setStatusText( _temp );
+}
+
+void FlxStatusBox::setStatusText( const std::string& text, float time_to_reset ) {
+	setStatusText( text.c_str(), time_to_reset );
+}
+
 void FlxStatusBox::setStatusText( const char *text, float time_to_reset ) {
+	Fl::remove_timeout( resetStatus_static, this );
 	copy_label( text );
+	redraw();
+	Fl::check();
 	Fl::add_timeout( time_to_reset, resetStatus_static, this );
 }
 
